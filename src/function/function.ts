78d78296cef3f3be760098d04474b4aa1af55be0
AdelQraft <24374323+AdelQraft@users.noreply.github.com>
2025-01-19 05:37:19 -0500

    work in progress

--- src/function/function.ts
+++ src/function/function.ts
@@ -1,96 +1,115 @@
-﻿import * as querystring from "node:querystring";
+﻿import {ProductInfo} from "./product_info";
 
 const axios = require("axios")
 const refreshPeriod_s = require("../taskpane/taskpane").refreshPeriod_s;
+
+const ITEM_ENDPOINT_URL = process.env.BACKEND_URL + "/v1/item"
 
 const ExcelFunctionError  = {
 	VALUE_ERROR: new CustomFunctions.Error(CustomFunctions.ErrorCode.invalidValue),
 	NOT_AVAILABLE: new CustomFunctions.Error(CustomFunctions.ErrorCode.notAvailable)
 }
 
-const superMarketNameToDefaultLocale = {
+const storeNameToDefaultLocale = {
 	"metro-canada": "en-CA"
 }
 
-const INFO_PARAM_ALLOWED_VALUES = new Set(["price", "protein-mass", "energy", "url"])
-function singleInfoIsValid(singleInfo: string) {
-	return singleInfo in INFO_PARAM_ALLOWED_VALUES;
+const hostnameToStoreName = {
+	"metro.ca": "metro-canada",
+	"www.metro.ca": "metro-canada"
 }
 
+const INFO_PARAM_NAME_ALLOWED_VALUES = new Set(["name", "url", "price-in-local-currency-slc", "count-id", "mass-g", "volume-L", "protein-mass-g", "energy-Cal"])
+function singleInfoIsValid(singleInfo: string) {
+	return INFO_PARAM_NAME_ALLOWED_VALUES.has(singleInfo);
+}
+
+// noinspection JSUnusedGlobalSymbols
 /**
- * Retrieve the price for the first item matching a search query given a supermarket name
+ * Retrieve the price for the first item matching a search query given a store name
  * @customfunction
- * @param superMarketName The name of the supermarket. Valid-values are: metro-canada.
- * @param searchQuery The search query.
- * @param quantity The quantity of the product.
- * @param quantityUnits The units of the quantity of the product (1, m, L and their multiples).
- * @param [infoToRetrieve] The information to retrieve, comma-separated, in order. Valid values are: price, protein-mass, energy, url. Price is in supermarket's currency. Protein mass and energy are always given in grams and food calories respectively.
- * @param [localeName] The standard locale name for the search query.
+ * @param url The URL of the product, from a known store.
+ * @param refQuantity The (reference) quantity of the product, possibly with its units. Standard units apply. standard multipliers are denoted by square brackets.
+ * @param refQuantityKind The kind of the (reference) quantity. Valid values are: price-in-local-currency, count, mass, volume, protein-mass, energy.
+ * @param [infoToRetrieve] The information to retrieve, comma-separated, in order. Valid values are: price-in-local-currency-slc, count-id, mass-g, volume-L, protein-mass-g, energy-Cal.
+ * @param [massToVolumeRatio] The ratio of mass to volume with its units. Standard units apply. standard multipliers are denoted by square brackets.
+ * @param [massToCountRatio] The ratio of mass to count with its units. Standard units apply. standard multipliers are denoted by square brackets.
  * @param [makePriceNegative] Make the price negative.
  * @param invocation The custom function handler.
  * @returns Information about the product in the specified information order.
  */
-export function getInfo(
-	superMarketName: string,
-	searchQuery: string,
-	quantity: number,
-	quantityUnits: string,
+export function getInfoFromUrl(
+	url: string,
+	refQuantity: string,
+	refQuantityKind: string,
+	massToCountRatio: string,
+	massToVolumeRatio: string,
 	infoToRetrieve: string,
-	localeName: string,
 	makePriceNegative: boolean,
 	invocation: CustomFunctions.StreamingInvocation<number[][]>
 ): void {
-	superMarketName = superMarketName.toLowerCase();
-	searchQuery = searchQuery.toLowerCase();
-	infoToRetrieve = infoToRetrieve?.toLowerCase();
-	localeName = localeName?.toLowerCase();
-
-	if (localeName == null) localeName = superMarketNameToDefaultLocale[superMarketName];
-
-	// abort if the current supermarket name is valid (known)
-	if (!(superMarketName in superMarketNameToDefaultLocale)) {
+	let hostname = new URL(url).hostname
+	if (hostname == null) {
 		invocation.setResult(ExcelFunctionError.VALUE_ERROR);
-		return;
+		return
 	}
 
 	let infoToRetrieveSplit: string[];
 	let reqParams = new URLSearchParams();
-	reqParams.append("supermarket-name", superMarketName);
-	if (infoToRetrieve == null) {
-		infoToRetrieveSplit = ["price", "protein-mass", "energy", "url"];
-		reqParams.append("info", "price");
-		reqParams.append("info", "protein-mass-g");
-		reqParams.append("info", "energy-Cal");
-		reqParams.append("info", "url");
-	} else {
-		infoToRetrieveSplit = infoToRetrieve.split(",");
-		for (const singleInfo in infoToRetrieveSplit) {
+	reqParams.append("url", url);
+	if (massToCountRatio) reqParams.append("mass-to-count-ratio", massToCountRatio)
+	if (massToVolumeRatio) reqParams.append("mass-to-volume-ratio", massToVolumeRatio)
+	if (infoToRetrieve) {
+		infoToRetrieveSplit = infoToRetrieve.split(",").map((value) => { return value.trim() });
+		for (const singleInfo of infoToRetrieveSplit) {
 			if (!singleInfoIsValid(singleInfo)) {
 				invocation.setResult(ExcelFunctionError.VALUE_ERROR);
 				return;
 			}
+
 			reqParams.append("info", singleInfo);
 		}
+	} else {
+		infoToRetrieveSplit = [
+			"price-in-local-currency-slc",
+			"protein-mass-g",
+			"energy-Cal",
+		];
+		for (const singleInfo of infoToRetrieveSplit) {
+			reqParams.append("info", singleInfo)
+		}
+
 	}
-	reqParams.append("search-query", searchQuery);
+	reqParams.append(refQuantityKind, refQuantity)
 
 	if (makePriceNegative == null) makePriceNegative = false;
 
 	let timeoutID: NodeJS.Timeout;
 	(function callApi() {
-		axios.get(process.env.BACKEND_URL, reqParams)
-			.then((r) => {
-				const info: ProductInfo = r.data;
-				invocation.setResult([
-					infoToRetrieveSplit.map((infoName) => {
-						let singleInfo = info[infoName];
-						if (infoName === "price" && makePriceNegative) singleInfo *= -1;
-						return singleInfo;
-					})
-				]);
+		axios.get(
+			ITEM_ENDPOINT_URL + '?' + reqParams.toString(),
+			{
+				timeout: 0
+			},
+		)
+			.then((resp: { data: any; }) => {
+				const info = new ProductInfo(resp.data);
+				invocation.setResult([infoToRetrieveSplit.map((infoName) => {
+					let singleInfo = info.getQuantitativeProperty(infoName).valueOf();
+					if (infoName === "price-in-local-currency-slc" && makePriceNegative) singleInfo *= -1;
+
+					return singleInfo;
+				})]);
 			})
-			.catch(() => {
-				invocation.setResult(ExcelFunctionError.NOT_AVAILABLE);
+			.catch((err) => {
+				if (err.response) {
+					if (err.response.status === 400 /* bad request */) {
+						invocation.setResult(ExcelFunctionError.VALUE_ERROR);
+					}
+				} else {
+					invocation.setResult(ExcelFunctionError.NOT_AVAILABLE);
+				}
+
 				return;
 			})
 
--- src/function/product_info.ts
+++ src/function/product_info.ts
@@ -1,6 +1,26 @@
-interface ProductInfo {
-	price_c: number;
-	proteinMass_cg: number;
-	energy_Cal: number;
-	url: string;
+export class ProductInfo {
+	name: String
+	url: String
+	priceInLocalCurrency_slc: Number
+	count_id: Number
+	mass_g: Number
+	volume_L: Number
+	proteinMass_g: Number
+	energy_Cal: Number
+
+	constructor(src: any) {
+		Object.assign(this, src)
+	}
+
+	getQuantitativeProperty(repr: String): Number {
+		switch (repr) {
+			case "price-in-local-currency-slc": return this.priceInLocalCurrency_slc
+			case "count-id": return this.count_id
+			case "mass-g": return this.mass_g
+			case "volume-L": return this.volume_L
+			case "protein-mass-g": return this.proteinMass_g
+			case "energy-Cal": return this.energy_Cal
+			default: throw new Error(`don't know how to parse \"${repr}\"`)
+		}
+	}
 }
